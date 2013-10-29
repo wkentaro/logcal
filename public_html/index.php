6745b08081748536dd3f8fb949164b6914be5ef8
Kentaro Wada <dev.kentaro.wada@gmail.com>
2013-10-29 23:56:09 +0900

    assets and dist

--- public_html/index.php
+++ public_html/index.php
@@ -15,20 +15,10 @@
 <title>ログカル｜LogCal- ログで時間管理</title>
 <link rel="index" href="/" title="LogCal">
 <link rel="shortcut icon" href="favicon.ico">
-<link href="assets/css/bootstrap.css" type="text/css" rel="stylesheet">
-<link href="assets/css/style.css" type="text/css" rel="stylesheet" media="screen">
-<script src="assets/js/jquery.js"></script>
-<script src="assets/js/bootstrap.js"></script>
-<script type="text/javascript">
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-42819052-1']);
-_gaq.push(['_trackPageview']);
-(function() {
- var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
- ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
- var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
- })();
-</script>
+<link href="/dist/css/bootstrap.css" type="text/css" rel="stylesheet">
+<link href="/assets/css/style.css" type="text/css" rel="stylesheet" media="screen">
+<script src="/assets/js/jquery.js"></script>
+<script src="/dist/js/bootstrap.js"></script>
 </head>
 
 <body>
