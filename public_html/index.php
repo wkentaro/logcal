a091dd9ef441404e02a8b3388a8457d489462dbe
Kentaro Wada <dev.kentaro.wada@gmail.com>
2013-11-03 01:48:29 +0900

    offering website

--- public_html/index.php
+++ public_html/index.php
@@ -1,163 +1,81 @@
 <!doctype html>
-<html lang="ja">
+<html lang="en">
 <head>
-<meta charset="utf-8">
-<meta http-equiv="content-Type" content="text/html; charset=utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="keywords" content="LogCal, ログカル, Googleカレンダー, ライフログ, ライフスタイル, 時間管理">
-<meta name="description" content="Googleカレンダーでライフログ、LogCalでライフ時間管理をしよう！">
-<meta property="og:title" content="LogCal">
-<meta property="og:type" content="website">
-<meta property="og:description" content="Googleカレンダーでライフログ、LogCalでライフ時間管理をしよう！">
-<meta property="og:url" content="http://logcal.ddo.jp">
-<meta property="og:image" content="http://logcal.ddo.jp/assets/img/logcal_large.png">
-<meta property="og:site_name" content="LogCal">
-<title>ログカル｜LogCal- ログで時間管理</title>
-<link rel="index" href="/" title="LogCal">
-<link rel="shortcut icon" href="favicon.ico">
-<link href="/dist/css/bootstrap.css" type="text/css" rel="stylesheet">
-<link href="/assets/css/style.css" type="text/css" rel="stylesheet" media="screen">
-<script src="/assets/js/jquery.js"></script>
-<script src="/dist/js/bootstrap.js"></script>
+  <meta charset="utf-8">
+  <meta http-equiv="content-Type" content="text/html; charset=utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta name="keywords" content="LogCal, Google Calendar, Gcal, Life Log, Life Style, Time Management">
+  <meta name="description" content="Manage your time using LogCal with Google Calendar.">
+  <meta property="og:title" content="LogCal">
+  <meta property="og:type" content="website">
+  <meta property="og:description" content="Manage your time using LogCal with Google Calendar.">
+  <meta property="og:url" content="http://logcal.ddo.jp/en/">
+  <meta property="og:image" content="http://logcal.ddo.jp/en/assets/img/logcal_large.png">
+  <meta property="og:site_name" content="LogCal">
+  <title>LogCal - Time Calculation</title>
+  <link rel="index" href="/" title="LogCal">
+  <link rel="shortcut icon" href="favicon.ico">
+  <link href="/dist/css/bootstrap.css" type="text/css" rel="stylesheet">
+  <style>
+    body{
+      padding-top: 20px;
+      padding-bottom: 20px;
+      /*background-color: #dddddd;*/
+      background-color: #5cb85c;
+    }
+    .jumbotron h1 {
+      color: #f8f8f8;
+    }
+    @media (min-width:767px) {
+      .jumbotron h1 {
+        font-size: 90px;
+        color: #f8f8f8;
+      }
+      .jumbotron p {
+        padding-top: 10px;
+        padding-bottom: 10px;
+      }
+    }
+    .navbar {
+      border-radius:4px;
+      margin-bottom: 20px;
+    }
+    .jumbotron {
+      background-color: #5cb85c;
+    }
+  </style>
 </head>
-
 <body>
-<div id="fb-root"></div>
-<script>
-(function(d, s, id) {
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) return;
- js = d.createElement(s); js.id = id;
- js.src = "//connect.facebook.net/ja_JP/all.js#xfbml=1";
- fjs.parentNode.insertBefore(js, fjs);
- }(document, 'script', 'facebook-jssdk'));
-</script>
-
-<div class="navbar navbar-inverse navbar-fixed-top">
 <div class="container">
-<a class="navbar-brand" href=".">LogCal</a>
-<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-responsive-collapse">
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-<span class="icon-bar"></span>
-</button>
-<div class="nav-collapse collapse navbar-responsive-collapse">
-<ul class="nav navbar-nav">
-<li class="active"><a href="http://logcal.ddo.jp">Home</a></li>
-<li><a href="prep/">準備をしよう</a></li>
-<li><a href="tips/">Tips</a></li>
-</ul>
-<ul class="nav navbar-nav pull-right">
-<li class="dropdown">
-<a href="#" class="dropdown-toggle" data-toggle="dropdown">Language</a>
-<ul class="dropdown-menu">
-<li><a href="en/">英語</a></li>
-<li><a href="zh/">中国語</a></li>
-</ul>
-</li>
-</ul>
+  <div class="navbar navbar-default navbar-inverse">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">LogCal</a>
+    </div>
+    <div class="navbar-collapse collapse">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">TOP</a></li>
+        <li><a href="#">PREP</a></li>
+        <li><a href="#">TIPS</a></li>
+      </ul>
+      <div class="navbar-form navbar-right">
+        <a href="#" class="btn btn-primary">Sign in Google</a>
+      </div>
+    </div>
+  </div>
+  <div class="jumbotron">
+    <div class="container">
+      <h1 class="text-center">LogCal</h1>
+      <p class="text-center">
+        Calculate the time of your life log in Google Calendar
+      </p>
+      <p class="text-right">offering soon..&nbsp;&nbsp;&nbsp;please vote»&nbsp;&nbsp;&nbsp;
+      <span class="pull-right"><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://logcal.info" data-text="LogCal | Calculate the time of your life log in Google Calendar" data-size="large">Tweet</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></p>
+    </div>
+  </div>
 </div>
-</div>
-</div>
-
-<div class="header masthead">
-<div class="container">
-<h1>LogCal</h1>
-<p class="text-default">Googleカレンダーのログで時間管理</p>
-<p><a href="logcal/" class="btn btn-warning btn-large">Use LogCal</a></p>
-</div>
-</div>
-
-<div class="share">
-<ul class="list-inline">
-<li><div class="fb-like" data-href="http://logcal.ddo.jp/" data-send="false" data-layout="button_count" data-width="450" data-show-faces="false"></div></li>
-<li><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://logcal.ddo.jp" data-text="LogCal - Googleカレンダー拡張アプリ。カレンダーでログをつけよう。" data-lang="ja" data-hashtags="LogCal">ツイート</a><script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script></li>
-</ul>
-</div>
-
-<div id="why"></div>
-<div class="page-header">
-<div class="container">
-<div class="introduction">
-<h1>Why <b>LogCal</b>?</h1>
-<p class="introduction-byline"></p>
-<div class="row">
-<div class="col-lg-4">
-<h2>今日から始めよう</h2>
-<div class="well well-large">
-<p><b>ログカル</b>は前日のカレンダーの<br/>ログから計算します。<br/>いつからでもすぐに始めることができます。</p>
-</div>
-</div>
-<div class="col-lg-4">
-<h2>時間の使い方</h2>
-<div class="well well-large">
-<p><b>ログカル</b>を使って、<br/>自分のライフスタイルを見直しましょう！<br/>現状の把握が改善の第一歩です。</p>
-</div>
-</div>
-<div class="col-lg-4">
-<h2>時間を意識する</h2>
-<div class="well well-large">
-<p>一つ一つの活動にかけた時間を<br/>意識しましょう！</p>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-
-<div id="how"></div>
-
-<div class="container">
-<div class="introduction">
-<h1>How to Use <b>LogCal</b>?</h1>
-<div class="row">
-<div class="col-lg-4">
-<h2>step1</h2>
-<p>Googleカレンダーで、<br/>Logを区別する分だけカレンダーを作成します。</p>
-<div class="row-fluid">
-<div class="col-lg-12">
-<div class="well well-large">
-<span class="label label-success">ライフ</span>
-<span class="label label-inverse">スクール</span>
-<span class="label label-info">アクティビティ</span>
-<span class="label label-warning">ホビー</span>
-</div>
-</div>
-</div>
-</div>
-<div class="col-lg-4">
-<h2>step2</h2>
-<p>カレンダーに逐一ログを入力します。<br/>（大変ですが一日頑張りましょう！）</p>
-<div class="row">
-<div class="col-lg-12">
-<div class="well well-large">
-<img id="introduction-img" src='assets/img/intro.png'>
-</div>
-</div>
-</div>
-</div>
-<div class="col-lg-4">
-<h2>step3</h2>
-<p>さぁ、ログの結果を確認しましょう！<br/>（前日のログが表示されます。）</p>
-<div class="row">
-<div class="col-lg-12">
-<div class="well well-large">
-<a href="logcal/" class="btn btn-primary">Let' Start!</a>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-</div>
-
-<div class="container">
-<footer class="footer-logcal">
-Created by Kentaro Wada
-©2013 UT Startup Gym
-</footer>
-</div>
-
-</body>
-</html>
 
